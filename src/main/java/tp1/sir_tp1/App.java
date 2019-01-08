a0f0e9bee0c75f93c3836b77334e5e3fc99fa730
Hibrahima <haidara99@gmail.com>
2019-01-08 11:40:44 +0100

    - initialized project

---
+++ src/main/java/tp1/sir_tp1/App.java
@@ -0,0 +1,13 @@
+package tp1.sir_tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
